0465dd483fa2bf98b5150d273aac016a2b785c73
yo1am1 <bigdiebam@gmail.com>
2023-06-09 01:18:23 +0300

    commit

--- exchange/tests.py
+++ exchange/tests.py
@@ -19,17 +19,16 @@ root = pathlib.Path(__file__).parent
 
 
 @pytest.fixture
-def mocked_response():
+def mocked():
     def inner(file_name):
         return json.load(open(root / "fixtures" / file_name))
 
     return inner
 
 
-
 @responses.activate
-def test_exchange_mono():
-    mocked_response = json.load(open(root / "fixtures/mono_response.json"))
+def test_exchange_mono(mocked):
+    mocked_response = mocked("mono_response.json")
     responses.get(
         "https://api.monobank.ua/bank/currency",
         json=mocked_response,
@@ -40,8 +39,8 @@ def test_exchange_mono():
 
 
 @responses.activate
-def test_privat_rate():
-    mocked_response = json.load(open(root / "fixtures/privat_response.json"))
+def test_privat_rate(mocked):
+    mocked_response = mocked("privat_response.json")
     responses.get(
         "https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11",
         json=mocked_response,
@@ -51,8 +50,8 @@ def test_privat_rate():
     assert e.pair.sell == 37.45318
 
 
-def test_vkurse_rate():
-    mocked_response = json.load(open(root / "fixtures/vkurse_response.json"))
+def test_vkurse_rate(mocked):
+    mocked_response = mocked("vkurse_response.json")
     responses.get(
         "http://vkurse.dp.ua/course.json",
         json=mocked_response,
@@ -62,8 +61,8 @@ def test_vkurse_rate():
     assert e.pair.sell == 37.1
 
 
-def test_nbu_rate():
-    mocked_response = json.load(open(root / "fixtures/nbu_response.json"))
+def test_nbu_rate(mocked):
+    mocked_response = mocked("nbu_response.json")
     responses.get(
         "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json",
         json=mocked_response,
@@ -73,8 +72,8 @@ def test_nbu_rate():
     assert e.pair.sell == 36.5686
 
 
-def test_currencyapi_rate():
-    mocked_response = json.load(open(root / "fixtures/currencyapi_response.json"))
+def test_currencyapi_rate(mocked):
+    mocked_response = mocked("currencyapi_response.json")
     responses.get(
         "https://currencyapi.net/api/v1/rates?base=USD&key=3c9b2a0c-7a3f-11eb-8b1b-7d4d8a7e0b7f",
         json=mocked_response,
